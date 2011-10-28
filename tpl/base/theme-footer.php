dfbdacf745c7e47c0809b1edf85594495432ca0d
shawnsandy <shawnsandy04@gmail.com>
2011-10-28 18:29:18 -0400

    Final Commit Fixed theme-footer

--- tpl/base/theme-footer.php
+++ tpl/base/theme-footer.php
@@ -8,7 +8,7 @@
 
 <section id="footer" class="container">
 
-        <div class="row show-on-desktops"><!-- container -->
+        <div class="row dotted-border"><!-- container -->
             <ul class="block-grid mobile four-up">
                 <li>
                     <?php if (!dynamic_sidebar('info-1')) : ?>
@@ -22,7 +22,6 @@
                         </nav>
 
                     <?php endif; ?>
-
                 </li>
                 <li>
                     <?php if (!dynamic_sidebar('info-2')) : ?>
@@ -51,19 +50,6 @@
                             Inceptos turpis convallis dolor etiam arcu sapien nibh fames, curabitur torquent eu nostra iaculis nisl blandit sit, ut ipsum elit iaculis orci litora primis.
                         </p>
                     <?php endif ?>
-                        <div class="icons">
-                                <!-- class content -->
-                                <?php //$cwp_feedburner = cwp_social::feedburner_subscriptions()  ?>
-                                <a href="http://feeds.feedburner.com/<?php the_author_meta('feedburner_page', 1); ?>" target="_blank">
-                                    <img src="<?php echo get_template_directory_uri() ?>/images/rss_blue.png" class="twitter-bird" />
-                                </a>
-                                <a href="<?php the_author_meta('facebook', 1); ?>" target="_blank">
-                                    <img src="<?php echo get_template_directory_uri() ?>/images/facebook_blue.png" class="twitter-bird" />
-                                </a>
-                                <a href="<?php the_author_meta('twitter', 1); ?>" target="_blank">
-                                    <img src="<?php echo get_template_directory_uri() ?>/images/twitter_blue.png" class="twitter-bird" />
-                                </a>
-                            </div>
                 </li>
             </ul>
 
@@ -71,7 +57,7 @@
         <div class="row">
             <div class="twelve columns">
                 <div id="copright-info">
-                    <p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a>.
+                    <p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>.
                         All Rights Reserved.
                         <br />
                     </p>
